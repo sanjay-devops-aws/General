1c6e6a6add41c9021cfb42956ae8a6fd3df377f6
sanjay-devops-aws <37576060+sanjay-devops-aws@users.noreply.github.com>
2021-02-12 21:32:20 +0530

    Update list-and-map.tf

--- Terraform/AWS/map and lookup function/list-and-map.tf
+++ Terraform/AWS/map and lookup function/list-and-map.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region     = "us-east-2"
  // region     = var.region
-  access_key = "AKIAWZNK45EKLVRWNKGA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
