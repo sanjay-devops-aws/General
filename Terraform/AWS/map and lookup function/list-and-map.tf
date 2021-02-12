464178ab6b54ee52b3be10040a51a1070ad8738d
sanjay <sanjaypn18@gmail.com>
2021-02-12 08:33:58 +0530

    count code

--- Terraform/AWS/map and lookup function/list-and-map.tf
+++ Terraform/AWS/map and lookup function/list-and-map.tf
@@ -1,28 +1,7 @@
 provider "aws" {
    region     = "us-east-2"
  // region     = var.region
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAWZNK45EKLVRWNKGA"
+  secret_key = "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"
 }
 
-
-resource "aws_instance" "myec2" {
-   ami = "ami-03d64741867e7bb94"
-   instance_type = var.types["us-west-2"]
-}
-
-variable "list" {
-  type = list
-  default = ["t2.micro","t2.nano","t2.medium"] 
-}
-
-variable "types" {
-  type = map 
-  default = {
-    us-east-2 = "t2.micro"
-    us-west-2 = "t2.medium"
-    
-
-  }
-  
-}
