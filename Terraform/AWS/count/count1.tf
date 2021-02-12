010e11a3ab62421c47f18518c3689ed26d6ebfcf
sanjay <sanjaypn18@gmail.com>
2021-02-12 08:49:42 +0530

    count code

--- Terraform/AWS/count/count1.tf
+++ Terraform/AWS/count/count1.tf
@@ -1,13 +1,19 @@
 provider "aws" {
    region     = "us-east-2"
  // region     = var.region
-  access_key = ""
-  secret_key = ""
+   access_key = "AKIAWZNK45EKLVRWNKGA"
+   secret_key = "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"
 }
-
 
 resource "aws_instance" "myec2" {
    ami = "ami-03d64741867e7bb94"
    instance_type = "t2.micro"
    count = 5
 }   
+
+resource "aws_iam_user" "ib" {
+  name = "loadbalancer"
+  count = 5
+  path  = "/system/"
+
+}
