d5fba4c5972bd6580589a78acd80a0c74ef4b0dd
sanjay <sanjaypn18@gmail.com>
2021-02-12 09:04:21 +0530

    count-index

--- Terraform/AWS/count/count2.tf
+++ Terraform/AWS/count/count2.tf
@@ -5,15 +5,8 @@ provider "aws" {
    secret_key = "/lWKgjAoBMFmD0rAwOBcYYco4yfdZ4I2f9vdG5cV"
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-03d64741867e7bb94"
-   instance_type = "t2.micro"
-   count = 5
-}   
-
 resource "aws_iam_user" "ib" {
-  name = "loadbalancer"
+  name = "loadbalancer.${count.index}"
   count = 5
   path  = "/system/"
-
 }
